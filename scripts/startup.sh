840e6e2c2990f157f8a1de7245bcf33232f62a02
Florent Detry <florent@igloo.be>
2014-01-24 16:03:58 +0100

    use full hostname of host

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo -e "[vhosts]\n$HOSTNAME:5984 = /registry/_design/app/_rewrite" >> /usr/local/etc/couchdb/local.d/npmjs-vhost.ini
-echo -e "127.0.0.1\t$HOSTNAME" >> /etc/hosts
-cat /opt/npmjs/kappa.json.default|sed -e "s/\${hostname}/$HOSTNAME/" > /opt/npmjs/kappa.json
+
+_HOSTNAME=`hostname -f`
+
+echo -e "[vhosts]\n$_HOSTNAME:5984 = /registry/_design/app/_rewrite" >> /usr/local/etc/couchdb/local.d/npmjs-vhost.ini
+echo -e "127.0.0.1\t$_HOSTNAME" >> /etc/hosts
+cat /opt/npmjs/kappa.json.default|sed -e "s/\${hostname}/$_HOSTNAME/" > /opt/npmjs/kappa.json
 couchdb -b; hapi -c /opt/npmjs/kappa.json & tail -f /usr/local/var/log/couchdb/couch.log
